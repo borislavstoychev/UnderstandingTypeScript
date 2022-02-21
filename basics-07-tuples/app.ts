6ff0dc8e6075727033f2326c405788c7273ea4b0
borislavstoychev <stoy4ew@gmail.com>
2022-02-21 16:40:40 +0200

    cutom types

--- basics-07-tuples/app.ts
+++ basics-07-tuples/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionDescription = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultCoversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultCoversion: ConversionDescription
 ) {
   let result: number | string;
   if (
