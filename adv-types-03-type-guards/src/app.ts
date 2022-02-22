774c7e15bc4bc826830b69273e379aee8099270a
borislavstoychev <stoy4ew@gmail.com>
2022-02-22 08:03:56 +0200

    overloads and optional chaining

--- adv-types-03-type-guards/src/app.ts
+++ adv-types-03-type-guards/src/app.ts
@@ -1,3 +1,36 @@
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add("Max", " Schwarz");
+result.split(" ");
+
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const userInput = undefined;
+
+const storedData = userInput ?? "DEFAULT";
+
+console.log(storedData);
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -17,18 +50,6 @@ const e1: ElevatedEmployee = {
   privileges: ["create-server"],
   startDate: new Date(),
 };
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combinable & Numeric;
-
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
 
 type UnknownEmployee = Employee | Admin;
 
